78e5da468a8342d15237a04b552ee87b01727326
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-06 20:50:58 +0530

    Services  aded

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -13,11 +14,33 @@ export class AddCourseComponent {
   duration=""
   date=""
 
+  constructor(private api:ApiService){}
+
 readValues=()=>
 {
   let data:any={ "courseTitle":this.courseTitle,"description":this.description,"venue":this.venue,"duration":this.duration,"date":this.date}
 
   console.log(data)
+
+  this.api.addCourse(data).subscribe(
+    (response:any)=>{
+      console.log(response)
+
+      if (response.status=="success") {
+        alert("Course Added Successfully")
+
+        this.courseTitle=""
+        this.description=""
+        this.venue=""
+        this.duration=""
+        this.date=""
+        
+      } else {
+        alert("Something went Wrong")
+        
+      }
+    }
+  )
 }
 
 }
